b8b7a123432bbc5f31eba8012cd0a0115ed73349
HannaGfrorer Gfrörer <hannagfrorer@gmail.com>
2023-03-30 11:41:48 +0000

    def get_word and def playword was added

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import word_list
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+
+def play(word):
+    word_completion = "-" * len(word)
+    guessed = False
+    guessed_letters = []
+    tries = 6
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word:").upper()
+        
