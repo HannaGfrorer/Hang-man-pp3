dbd9e7cfd051148752d0f1581b4db194c75a7d29
HannaGfrorer Gfrörer <hannagfrorer@gmail.com>
2023-03-31 09:34:49 +0000

    did a neve hang man

--- run.py
+++ run.py
@@ -19,35 +19,35 @@ def play(word):
     print("\n")
     while not guessed and tries > 0:
         guess = input("Please guess a letter or word: ").upper()
-    if len(guess) == 1 and guess.isalpha():
-        if guess in guessed_letters:
-            print("You already guessed the letter", guess)
-    elif guess not in word:
-        print(guess, "is not in the word.")
-        tries -= 1
-        guessed_letters.append(guess)
-    else:
-        print("Good job,", guess, "is in the word!")
-        guessed_letters.append(guess)
-        word_as_list = list(word_completion)
-        indices = [i for i, letter in enumerate(word) if letter == guess]
-    for index in indices:
-        word_as_list[index] = guess
-        word_completion = "".join(word_as_list)
-    if "_" not in word_completion:
-        guessed = True
-    elif len(guess) == len(word) and guess.isalpha():
-        if guess in guessed_words:
-            print("You already guessed the word", guess)
-    elif guess != word:
-        print(guess, "is not the word.")
-        tries -= 1
-        guessed_words.append(guess)
-    else:
-        guessed = True
-        word_completion = word
-    else:
-        print("Not a valid guess.")
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Not a valid guess.")
         print(display_hangman(tries))
         print(word_completion)
         print("\n")
@@ -58,77 +58,64 @@ def play(word):
 
 
 def display_hangman(tries):
-    stages = [  # final state: head, torso, both arms, and both legs
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # head, torso, both arms, and one leg
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / 
-                   -
-                """,
-                # head, torso, and both arms
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |      
-                   -
-                """,
-                # head, torso, and one arm
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |     
-                   -
-                """,
-                # head and torso
-                """
-                   --------
-                   |      |
-                   |      O
-                   |      |
-                   |      |
-                   |     
-                   -
-                """,
-                # head
-                """
-                   --------
-                   |      |
-                   |      O
-                   |    
-                   |      
-                   |     
-                   -
-                """,
-                # initial empty state
-                """
-                   --------
-                   |      |
-                   |      
-                   |    
-                   |      
-                   |     
-                   -
-                """
-    ]
+    stages = {
+        0: """
+                ___________
+               | /        | 
+               |/        ( )
+               |          |
+               |         / \\
+               |
+           """,
+        1: """
+                ___________
+               | /        | 
+               |/        ( )
+               |          |
+               |         / 
+               |
+            """,
+        2: """
+                ___________
+               | /        | 
+               |/        ( )
+               |          |
+               |          
+               |
+            """,
+        3: """
+                ___________
+               | /        | 
+               |/        ( )
+               |          
+               |          
+               |
+            """,
+        4: """
+                ___________
+               | /        | 
+               |/        
+               |          
+               |          
+               |
+            """,
+        5: """
+                ___________
+               | /        
+               |/        
+               |          
+               |          
+               |
+            """,
+        6: """
+               |
+               |
+               |
+               |
+               |
+            """,
+        7: "",
+    }
     return stages[tries]
 
 
