84f9b626ade4d5321b12fbed766730addd4065b1
HannaGfrorer Gfrörer <hannagfrorer@gmail.com>
2023-03-31 07:54:29 +0000

    change words

--- run.py
+++ run.py
@@ -17,11 +17,11 @@ def play(word):
     print(display_hangman(tries))
     print(word_completion)
     print("\n")
- while not guessed and tries > 0:
-     guess = input("Please guess a letter or word: ").upper()
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper()
     if len(guess) == 1 and guess.isalpha():
-    if guess in guessed_letters:
-        print("You already guessed the letter", guess)
+        if guess in guessed_letters:
+            print("You already guessed the letter", guess)
     elif guess not in word:
         print(guess, "is not in the word.")
         tries -= 1
@@ -31,30 +31,30 @@ def play(word):
         guessed_letters.append(guess)
         word_as_list = list(word_completion)
         indices = [i for i, letter in enumerate(word) if letter == guess]
-        for index in indices:
+    for index in indices:
         word_as_list[index] = guess
         word_completion = "".join(word_as_list)
-       if "_" not in word_completion:
-            guessed = True
-        elif len(guess) == len(word) and guess.isalpha():
-            if guess in guessed_words:
-                print("You already guessed the word", guess)
-            elif guess != word:
-                print(guess, "is not the word.")
-                tries -= 1
-                guessed_words.append(guess)
-            else:
-                guessed = True
-                word_completion = word
-        else:
-            print("Not a valid guess.")
+    if "_" not in word_completion:
+        guessed = True
+    elif len(guess) == len(word) and guess.isalpha():
+        if guess in guessed_words:
+            print("You already guessed the word", guess)
+    elif guess != word:
+        print(guess, "is not the word.")
+        tries -= 1
+        guessed_words.append(guess)
+    else:
+        guessed = True
+        word_completion = word
+    else:
+        print("Not a valid guess.")
         print(display_hangman(tries))
         print(word_completion)
         print("\n")
     if guessed:
         print("Congrats, you guessed the word! You win!")
     else:
-        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
+        print("Sorry, The word was " + word + ". Maybe next time!")
 
 
 def display_hangman(tries):
